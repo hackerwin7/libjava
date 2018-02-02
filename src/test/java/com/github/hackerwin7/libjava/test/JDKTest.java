ba32b1a78252319cc507a247de4510754d38bbdd
hackerwin7 <hackerswin7@gmail.com>
2018-02-02 20:21:52 +0800

    add test for concurrent etc..

--- src/test/java/com/github/hackerwin7/libjava/test/JDKTest.java
+++ src/test/java/com/github/hackerwin7/libjava/test/JDKTest.java
@@ -1,5 +1,9 @@
 package com.github.hackerwin7.libjava.test;
 
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hackerwin7
@@ -8,11 +12,70 @@ package com.github.hackerwin7.libjava.test;
  * Desc:
  */
 public class JDKTest {
-    public static void main(String[] args) {
-        ceilTest();
+    public static void main(String[] args) throws Exception {
+        threadReuse();
     }
 
     private static void ceilTest() {
         System.out.println(Math.ceil(3 * 0.0000001));
+    }
+
+    private static void compareMap() {
+        Map<Integer, List<String>> m1 = new HashMap<>();
+        m1.put(1, Arrays.asList("1", "11", "111"));
+        Map<Integer, List<String>> m2 = new HashMap<>();
+        m2.put(1, Arrays.asList("1", "11", "111"));
+        Map<Integer, List<String>> m3 = new HashMap<>();
+        m3.put(1, Arrays.asList("1", "12", "123"));
+        System.out.println(m1.equals(m2));
+        System.out.println(m1.equals(m3));
+    }
+
+    private static void executeAndSubmit() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("execute:" + new Date());
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        });
+        System.out.println("=================================================================");
+        ExecutorService submitter = Executors.newSingleThreadExecutor();
+        submitter.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("submit: " + new Date());
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        });
+
+    }
+
+    private static void threadReuse() throws Exception {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("end.");
+            }
+        });
+        thread.start();
+        Thread.sleep(2000);
+        if (!thread.isAlive()) {
+            System.out.println("restart");
+            thread.start();
+        }
     }
 }
