46f734160634d5c6d0db33ba7c693b7f3f08b434
hackerwin7 <hackerswin7@gmail.com>
2018-08-22 17:17:56 +0800

    add inet address sample

--- src/main/java/com/github/hackerwin7/libjava/exec/Tester.java
+++ src/main/java/com/github/hackerwin7/libjava/exec/Tester.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.hackerwin7.libjava.common.Utils;
 import com.github.hackerwin7.libjava.http.HttpExecutor;
 
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 
 /**
@@ -15,17 +16,35 @@ import java.util.HashMap;
  */
 public class Tester {
     public static void main(String[] args) throws Exception {
+        inetAddrTest("wenyuhe.jdq.jd.local", 9888);
+    }
+
+    public static void httpTest() throws Exception {
         HttpExecutor executor = HttpExecutor.create();
         HashMap<String, String> params = new HashMap<>();
         params.put("appId", "k8s.kafka.monitor");
-        params.put("token", "4UHQX4227ZF3YCITIJYDJIBYDVM43NT22VY25HY");
+        params.put("token", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         params.put("time", String.valueOf(System.currentTimeMillis()));
         ObjectNode data = Utils.createJsonNode();
         data.put("usedId", 0);
         data.put("type", 2);
         params.put("data", Utils.json2String(data));
-        String ret = executor.doPost("http://jrdw.bdp.jd.local/jrdw/clusterManage/api/getClusterList.ajax", params);
+        String ret = executor.doPost("http://xxx/xxx/xx/getClusterList.ajax", params);
         System.out.println(Utils.string2Json(ret));
+    }
+
+    public static void inetAddrTest(String host, int port) throws Exception {
+
+        long sleepTime = 0;
+        long sleepUnit = 5000;
+        while (sleepTime < 1440 * 60 * 1000) {
+            InetSocketAddress isa = new InetSocketAddress(host, port);
+            System.out.println("hostName: " + isa.getHostName() + ", hostString = " + isa.getHostString() + ", host addr = " + isa.getAddress().getHostAddress());
+            System.out.println("Sleeping...");
+            Thread.sleep(sleepUnit);
+            sleepTime += sleepUnit;
+        }
+
     }
 
 }
