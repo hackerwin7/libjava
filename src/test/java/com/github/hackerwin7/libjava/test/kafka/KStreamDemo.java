51c10bdd109c81e8eaa4a79018210c6b4ddba887
hackerwin7 <hackerswin7@gmail.com>
2019-02-18 21:26:39 +0800

    shorten streaming time process of demo

--- src/test/java/com/github/hackerwin7/libjava/test/kafka/KStreamDemo.java
+++ src/test/java/com/github/hackerwin7/libjava/test/kafka/KStreamDemo.java
@@ -23,13 +23,14 @@ import java.util.Properties;
  */
 public class KStreamDemo {
     public static void main(String[] args) {
-
+        demo();
     }
 
     public static void demo() {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0); // shorten stream time (latency) in demo
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
