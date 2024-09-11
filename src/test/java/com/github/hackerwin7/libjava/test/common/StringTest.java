d63b7a10deaab052981f1b63947fc30035d90bcc
hackerwin7 <hackerswin7@gmail.com>
2024-09-11 19:04:02 +0800

    sync

--- src/test/java/com/github/hackerwin7/libjava/test/common/StringTest.java
+++ src/test/java/com/github/hackerwin7/libjava/test/common/StringTest.java
@@ -2,9 +2,15 @@ package com.github.hackerwin7.libjava.test.common;
 
 import com.github.hackerwin7.libjava.utils.EscapeUtils;
 import com.google.common.collect.Sets;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.byted.security.zti.jwt.shaded.javax.xml.bind.DatatypeConverter;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +23,80 @@ import java.util.Set;
 @Slf4j
 public class StringTest {
   public static void main(String[] args) {
-    testStartWith();
+    hexString();
+  }
+
+  public static void hexString() {
+    String s = "\\x00\\x00\\x01\\x91\\x92\\xe9)\\xb4";
+    System.out.println(s);
+    System.out.println(unHexBytes(s));
+  }
+
+  public static String unHexBytes(String arg) {
+    List<Byte> bytesList = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < arg.length(); i++) {
+      if (arg.charAt(i) == '\\' && arg.charAt(i + 1) == 'x') {
+        int hexInt = Integer.parseInt(arg.substring(i + 2, i + 4), 16);
+        char c = (char) hexInt;
+        sb.append(c);
+//        System.out.println("hex char: " + c);
+        bytesList.add((byte) hexInt);
+        i += 3;
+      } else {
+        char c = arg.charAt(i);
+        sb.append(c);
+//        System.out.println("char: " + c);
+        bytesList.add((byte) arg.charAt(i));
+      }
+    }
+    byte[] bytes = new byte[bytesList.size()];
+    for (int i = 0; i < bytesList.size(); i++) {
+      bytes[i] = bytesList.get(i);
+    }
+//    System.out.println("string builder: " + sb);
+    return new String(bytes);
+  }
+
+  public static String unHex(String arg) {
+    StringBuilder str = new StringBuilder();
+    for (int i = 0; i < arg.length(); i++) {
+      if (arg.charAt(i) == '\\' && arg.charAt(i + 1) == 'x') {
+        str.append((char) Integer.parseInt(arg.substring(i + 2, i + 4), 16));
+        i += 3;
+      } else {
+        str.append(arg.charAt(i));
+      }
+    }
+    return str.toString();
+  }
+
+  public static void escapeString() {
+    String s = "\\x00\\x00\\x01\\x91\\x92\\xe9)\\xb4";
+    System.out.println(s);
+    System.out.println(StringEscapeUtils.unescapeJava(s));
+    byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+    String s1 = new String(bytes);
+    System.out.println(s1);
+    System.out.println(StringEscapeUtils.unescapeJava(s1));
+    String s2 = "���)�";
+    System.out.println(s2);
+    byte[] bytes2 = s2.getBytes();
+    System.out.println(new String(bytes2));
+    System.out.println(StringEscapeUtils.unescapeJava(new String(bytes2)));
+  }
+
+  public static void testSplit() {
+    String s = "id";
+    String[] arr = s.split(",\\s*");
+    System.out.println(Arrays.toString(arr));
+  }
+
+  public static void testStringBytes() {
+    String s = "\\x00\\x00\\x01\\x91\\x93\\x16\\xa0]";
+    byte[] bytes = s.getBytes();
+    System.out.println(s);
+    System.out.println(new String(bytes));
   }
 
   public static void testStartWith() {
