3aa7d817672229d59224e3dd41e1cc20ca33ad5b
hackerwin7 <hackerswin7@gmail.com>
2025-01-11 17:42:58 +0800

    sync

--- src/test/java/com/github/hackerwin7/libjava/test/common/EnumTest.java
+++ src/test/java/com/github/hackerwin7/libjava/test/common/EnumTest.java
@@ -9,10 +9,36 @@ import java.util.stream.Collectors;
  **/
 public class EnumTest {
   public static void main(String[] args) {
-    test1();
+    testHttpMethods();
+  }
+
+  public static void testHttpMethods() {
+    System.out.println(HTTPMethods.GET.value());
+    System.out.println(HTTPMethods.GET.name());
+    System.out.println(HTTPMethods.GET.ordinal());
+    System.out.println(HTTPMethods.GET.getClass().getName());
   }
 
   public static void test1() {
     System.out.println(Arrays.stream(EnumInstance.values()).map(EnumInstance::toString).collect(Collectors.toList()));
+  }
+
+  public enum HTTPMethods {
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    DELETE;
+
+    private HTTPMethods() {
+    }
+
+    public String value() {
+      return this.name();
+    }
+
+    public static HTTPMethods fromValue(String v) {
+      return valueOf(v);
+    }
   }
 }
--- src/test/java/com/github/hackerwin7/libjava/test/common/ExceptionTest.java
+++ src/test/java/com/github/hackerwin7/libjava/test/common/ExceptionTest.java
@@ -6,7 +6,20 @@ package com.github.hackerwin7.libjava.test.common;
  **/
 public class ExceptionTest {
   public static void main(String[] args) {
-    runtimeExceptionTest();
+    tryResourceCatchTest();
+  }
+
+  public static void tryResourceCatchTest() {
+    try (AutoCloseable c1 = getStringThrow()) {
+      throw new RuntimeException("test");
+    } catch (Exception e) {
+      System.out.println("catch exception " + e.getClass().getName());
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static AutoCloseable getStringThrow() {
+    throw new RuntimeException("get string throw");
   }
 
   public static void runtimeExceptionTest() {
